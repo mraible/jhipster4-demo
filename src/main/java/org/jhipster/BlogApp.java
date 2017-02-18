0aeb3b658c3a129c6bf5fb06329fecd557f74638
Matt Raible <matt@raibledesigns.com>
2017-02-18 15:55:08 -0700

    Upgrade to JHipster 4.0.5

--- src/main/java/org/jhipster/BlogApp.java
+++ src/main/java/org/jhipster/BlogApp.java
@@ -1,5 +1,6 @@
 package org.jhipster;
 
+import org.jhipster.config.ApplicationProperties;
 import org.jhipster.config.DefaultProfileUtil;
 
 import io.github.jhipster.config.JHipsterConstants;
@@ -22,7 +23,7 @@ import java.util.Collection;
 
 @ComponentScan
 @EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
-@EnableConfigurationProperties(LiquibaseProperties.class)
+@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 public class BlogApp {
 
     private static final Logger log = LoggerFactory.getLogger(BlogApp.class);
@@ -63,13 +64,19 @@ public class BlogApp {
         SpringApplication app = new SpringApplication(BlogApp.class);
         DefaultProfileUtil.addDefaultProfile(app);
         Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
         log.info("\n----------------------------------------------------------\n\t" +
                 "Application '{}' is running! Access URLs:\n\t" +
-                "Local: \t\thttp://localhost:{}\n\t" +
-                "External: \thttp://{}:{}\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
                 "Profile(s): \t{}\n----------------------------------------------------------",
             env.getProperty("spring.application.name"),
+            protocol,
             env.getProperty("server.port"),
+            protocol,
             InetAddress.getLocalHost().getHostAddress(),
             env.getProperty("server.port"),
             env.getActiveProfiles());
